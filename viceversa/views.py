4f3ca208cce498aede76503d45f83203a1916d94
Your Name <you@example.com>
2023-09-19 15:37:48 +0500

    Adding words counter

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,3 +1,4 @@
+from itertools import count
 from django.shortcuts import render
 
 def home(request):
@@ -5,5 +6,7 @@ def home(request):
 
 def reverse(request):
     getting_text = request.GET["usertext"]
+    count_words= getting_text.split()
+    num_of_words = len(count_words)
     reversed_text = getting_text[::-1]
-    return render(request, "reverse.html", {"usertext": getting_text, "reversedtext": reversed_text})
+    return render(request, "reverse.html", {"usertext": getting_text, "reversedtext": reversed_text, "num_of_words": num_of_words})
